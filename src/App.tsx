ef768075069f859de54c9f9a20237ba49d432177
Moinak Kuila <88288039+Spoidarman@users.noreply.github.com>
2025-05-22 17:45:21 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Layout from './components/Layout/Layout';
+import Dashboard from './components/pages/Dashboard';
+
+function App() {
+  return (
+    <Layout>
+      <Dashboard />
+    </Layout>
+  );
+}
+
+export default App;
